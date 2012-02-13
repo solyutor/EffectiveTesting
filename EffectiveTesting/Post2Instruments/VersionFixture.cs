797831ddf0cb7a7e3193b01010e772b58dcba26b
j.soldatenkov <yurysold@googlemail.com>
2012-02-14 00:37:25 +0400

    Initial commit

---
+++ EffectiveTesting/Post2Instruments/VersionFixture.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace EffectiveTesting.Post2Instruments
+{
+    [TestFixture]
+    public class VersionFixture
+    {
+        [Test]
+        public void Constructor_parses_string_properly()
+        {
+            var version = new Version("1.2.330.400");
+
+            //Обратите внимание, все сообщения консоли будут выведены в GUI NUnit
+            //Часто таким способом полезно "логгировать" прохождене теста
+            Console.WriteLine("Major={0} Minor={1} Build={2} Revision = {3}", version.Major, version.Minor, version.Build, version.Revision);
+
+            Assert.AreEqual(1, version.Major);
+            Assert.AreEqual(2, version.Minor);
+            Assert.AreEqual(330, version.Build);
+            Assert.AreEqual(400, version.Revision);
+        }    
+    
+        [Test]
+        public void Constructor_parses_string_properly_with_fluent_API()
+        {
+            var version = new Version("1.2.330.400");
+
+            //Обратите внимание, все сообщения консоли будут выведены в GUI NUnit
+            //Часто таким способом полезно "логгировать" прохождене теста
+            Console.WriteLine("Major={0} Minor={1} Build={2} Revision = {3}", version.Major, version.Minor, version.Build, version.Revision);
+
+            Assert.That(version.Major, Is.EqualTo(1));
+            Assert.That(version.Minor, Is.EqualTo(2));
+            Assert.That(version.Build, Is.EqualTo(330));
+            Assert.That(version.Revision, Is.EqualTo(400));
+        }
+
+        [Test]
+        public void Constructor_parses_string_properly_in_the_wrong_way()
+        {
+            var version = new Version("1.2.330.400");
+
+            //Обратите внимание, все сообщения консоли будут выведены в GUI NUnit
+            //Часто таким способом полезно "логгировать" прохождене теста
+            Console.WriteLine("Major={0} Minor={1} Build={2} Revision = {3}", version.Major, version.Minor, version.Build, version.Revision);
+        
+            //Так делать очень плохо, т.к.. в итоге сообщение об ошибке будет невыразительным
+            Assert.IsTrue(version.Major == 1);
+            Assert.IsTrue(version.Minor == 2);
+            Assert.IsTrue(version.Build == 330);
+            Assert.IsTrue(version.Revision == 401);
+        }
+    }
+}
---
+++ EffectiveTesting/Version.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace EffectiveTesting
+{
+    public class Version
+    {
+        private readonly int _major;
+        private readonly int _minor;
+        private readonly int _build;
+        private readonly int _revision;
+
+        public Version(string version)
+        {
+            var versionNumbers = version.Split('.');
+
+            _major = Convert.ToInt32(versionNumbers[0]);
+            _minor = Convert.ToInt32(versionNumbers[1]);
+            _build = Convert.ToInt32(versionNumbers[2]);
+            _revision = Convert.ToInt32(versionNumbers[3]);
+        }
+
+        public int Revision
+        {
+            get { return _revision; }
+        }
+
+        public int Build
+        {
+            get { return _build; }
+        }
+
+        public int Minor
+        {
+            get { return _minor; }
+        }
+
+        public int Major
+        {
+            get { return _major; }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}.{1}.{2}.{3}", Major, Minor, Build, Revision);
+        }
+    }
+}
