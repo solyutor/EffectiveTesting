ff74f839343d4003b8c105590fdc3f531adfaf6f
j.soldatenkov <yurysold@googlemail.com>
2012-03-14 00:29:00 +0400

    Added Moq blog post code

---
+++ EffectiveTesting/Post4Mocking/OrderListPresenterFixture.cs
@@ -0,0 +1,59 @@
+﻿using Moq;
+using NHibernate;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace EffectiveTesting.Post4Mocking
+{
+    [TestFixture]
+    public class OrderListPresenterFixture
+    {
+[Test] 
+public void On_start_puts_in_view_some_orders_behaviour_style()
+{
+    //Создаём двойник интерфейса IOrderView
+    var viewMock = new Mock<IOrderView>();
+
+    var order = new Order(10);
+    //Создаём двойник интерфейса ISession
+    var sessionMock = new Mock<ISession>();
+    sessionMock
+        //При вызове метода Get<Order> с аргументов 10
+        .Setup(x => x.Get<Order>(It.Is<long>(id => id.Equals(10))))
+        //И вернём 
+        .Returns(order);
+
+    var presenter = new OrderPresenter(sessionMock.Object, viewMock.Object);
+
+    presenter.Start(10);
+
+    viewMock.VerifySet(x => x.Visible = true);
+    viewMock.VerifySet(x => x.Order = order);
+}
+        
+[Test] 
+public void On_start_puts_in_view_some_orders_state_style()
+{
+    var viewMock = new Mock<IOrderView>();
+    //Устанавливаем поведение всех свойств по-умолчанию. 
+    viewMock.SetupAllProperties();
+
+    var order = new Order(10);
+
+    var sessionMock = new Mock<ISession>();
+    sessionMock
+        //При вызове метода Get<Order> с аргументов 10
+        .Setup(x => x.Get<Order>(It.Is<long>(id => id.Equals(10))))
+        //И вернём 
+        .Returns(order);
+
+    var presenter = new OrderPresenter(sessionMock.Object, viewMock.Object);
+
+    presenter.Start(10);
+
+    viewMock.Object.Satisfy(view =>
+                            view.Visible == true &&
+                            view.Order == order);
+}
+    }
+}
---
+++ EffectiveTesting/Post4Mocking/OrderPresenter.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using NHibernate;
+
+namespace EffectiveTesting.Post4Mocking
+{
+    public class Order
+    {
+        public long Id { get; private set; }
+
+        public Order(long id)
+        {
+            Id = id;
+        }
+
+        //skipped
+    }
+
+    public interface IOrderView
+    {
+        Order Order { get; set; }
+        bool Visible { get; set; }
+    }
+    
+    public class OrderPresenter
+    {
+        private readonly ISession _session;
+        private readonly IOrderView _view;
+
+        public OrderPresenter(ISession session, IOrderView view)
+        {
+            _session = session;
+            _view = view;
+        }
+
+        public void Start(long id)
+        {
+            _view.Order = _session.Get<Order>(id);
+            _view.Visible = true;
+        }
+    }
+}
