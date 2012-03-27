8a3844b79f83f54f04de565d0e54980d78d811ae
j.soldatenkov <yurysold@googlemail.com>
2012-03-27 22:19:05 +0400

    Modified tests for the sake of measure second run time

--- EffectiveTesting/Post5ByeByeMocks/TestWithMocks.cs
+++ EffectiveTesting/Post5ByeByeMocks/TestWithMocks.cs
@@ -67,6 +67,66 @@ namespace EffectiveTesting.Post5ByeByeMocks
             //Assert.
             Assert.That(price.LocalAmount, Is.EqualTo(325));
             repo.VerifyAll();
+        }        
+        
+        [Test]
+        public void Ensure_valid_prices_would_be_updated2()
+        {
+            //Arrange.
+            var currentDate = DateTime.Today;
+            var price = new Price(10, currentDate.AddDays(-1));
+            var pricesToUpdate = new List<Price>{ price };
+        
+            var repo = new MockRepository(MockBehavior.Loose);
+        
+            var transactionMock = repo.Create<ITransaction>();
+            transactionMock
+                .Setup(x => x.Commit())
+                .Verifiable();
+
+            var queryMock = repo.Create<IQuery>();
+            queryMock
+                .Setup(x => x.SetParameter("currentDate", currentDate))
+                .Returns(queryMock.Object);
+            queryMock
+                .Setup(x => x.List<Price>())
+                .Returns(pricesToUpdate);
+        
+            var sessionMock = repo.Create<ISession>();
+
+            sessionMock
+                .Setup(x => x.BeginTransaction())
+                .Returns(transactionMock.Object)
+                .Verifiable();
+        
+            sessionMock
+                .Setup(x => x.CreateQuery("from Price p where p.ValidFrom <= :currentDate and :currentDate <= p.ValidTo"))
+                .Returns(queryMock.Object);
+
+            var sessionFactoryMock = repo.Create<ISessionManager>();
+            sessionFactoryMock
+                .Setup(x => x.OpenSession())
+                .Returns(sessionMock.Object);
+        
+            var clockMock = repo.Create<IClock>();
+            clockMock
+                .SetupGet( x => x.Today)
+                .Returns(currentDate);
+        
+            var rateProviderMock = repo.Create<IRateProvider>();
+        
+            rateProviderMock
+                .Setup(x => x.GetRateOn(currentDate))
+                .Returns(32.5m);
+
+            var task = new UpdatePricesTask(sessionFactoryMock.Object, clockMock.Object, rateProviderMock.Object);
+        
+            //Act.
+            task.Run();
+        
+            //Assert.
+            Assert.That(price.LocalAmount, Is.EqualTo(325));
+            repo.VerifyAll();
         }
     
         [Test]
--- EffectiveTesting/Post5ByeByeMocks/TestWithoutMocks.cs
+++ EffectiveTesting/Post5ByeByeMocks/TestWithoutMocks.cs
@@ -8,34 +8,83 @@ namespace EffectiveTesting.Post5ByeByeMocks
     [TestFixture]
     public class TestWithoutMocks
     {
+        private Configuration _configuration;
+        private SQLiteInMemoryTestHelper _testHelper;
+        private EnhimaSessionManager _sessionManager;
+
+        [TestFixtureSetUp]
+        public void TestFixtureSetup()
+        {
+            _configuration = new Configuration();//.DataBaseIntegration(db => db.LogSqlInConsole = true);
+
+            _configuration.ConfigureSQLiteInMemory();
+            _configuration.MapEntities(From.ThisApplication());
+            _testHelper = new SQLiteInMemoryTestHelper(_configuration);
+            
+
+            _sessionManager = new EnhimaSessionManager(_testHelper);
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            _testHelper.CreateSchema();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _testHelper.DropSchema();
+        }
+
         [Test]
         public void Ensure_valid_prices_would_be_updated()
         {
-            var configuration = new Configuration().DataBaseIntegration(db => db.LogSqlInConsole = true );
-
-            configuration.ConfigureSQLiteInMemory();
-            configuration.MapEntities(From.ThisApplication());
-            var testHelper = new SQLiteInMemoryTestHelper(configuration);
-            testHelper.CreateSchema();
-
-            var manager = new EnhimaSessionManager(testHelper);
-
             var today = Clock.FixedTime.Today;
             var yesterday = today.AddDays(-1);
 
             var priceToUpdate = new Price(10, yesterday) { ValidFrom = yesterday, ValidTo = today };
             var priceToSkip = new Price(20, yesterday) { ValidFrom = yesterday, ValidTo = yesterday };
 
-            testHelper.Persist(priceToUpdate, priceToSkip);
+            _testHelper.Persist(priceToUpdate, priceToSkip);
 
             var rateProviderStub = new RateProviderStub();
 
-            var task = new UpdatePricesTask(manager, Clock.FixedTime, rateProviderStub);
+            var task = new UpdatePricesTask(_sessionManager, Clock.FixedTime, rateProviderStub);
 
             task.Run();
 
-            var savedPriceToUpdate = testHelper.Load<Price>(priceToUpdate.Id);
-            var savedPriceToSkip = testHelper.Load<Price>(priceToSkip.Id);
+            var savedPriceToUpdate = _testHelper.Load<Price>(priceToUpdate.Id);
+            var savedPriceToSkip = _testHelper.Load<Price>(priceToSkip.Id);
+
+            savedPriceToUpdate.Satisfy(x =>
+                                       x.LocalAmount == 325m &&
+                                       x.LastUpdated == Clock.FixedTime.Today);
+            
+            savedPriceToSkip.Satisfy(x =>
+                                     x.LocalAmount == 0 &&
+                                     x.LastUpdated == yesterday);
+        }        
+        
+        [Test]
+        public void Ensure_valid_prices_would_be_updated2()
+        {
+            var today = Clock.FixedTime.Today;
+            var yesterday = today.AddDays(-1);
+
+            var priceToUpdate = new Price(10, yesterday) { ValidFrom = yesterday, ValidTo = today };
+            var priceToSkip = new Price(20, yesterday) { ValidFrom = yesterday, ValidTo = yesterday };
+
+            _testHelper.Persist(priceToUpdate, priceToSkip);
+
+            var rateProviderStub = new RateProviderStub();
+
+            var task = new UpdatePricesTask(_sessionManager, Clock.FixedTime, rateProviderStub);
+
+            task.Run();
+
+            var savedPriceToUpdate = _testHelper.Load<Price>(priceToUpdate.Id);
+            var savedPriceToSkip = _testHelper.Load<Price>(priceToSkip.Id);
 
             savedPriceToUpdate.Satisfy(x =>
                                        x.LocalAmount == 325m &&
